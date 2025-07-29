beab629a0a76f28d4858b10b3dfef35d81c00a0c
Bruce <peichunzhou1006@gmail.com>
2025-07-29 19:53:18 +1000

    init dotnet di example

---
+++ dotnet/di_keyed_service/Controllers/TestController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace di_keyed_service.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        
+    }
+}

82b24685b52596332be495814d7c388295ddb935
Bruce <peichunzhou1006@gmail.com>
2025-07-30 22:43:16 +1000

    Rebuilt mssql with full text search, added create full text index migration
    * Dockerfile source: https://gist.github.com/pbthorste/2dd302939ad84ab26932e9e18b7428d8

---
+++ dotnet/boolean_search/API/Data/Migrations/20250730115805_AddFulLTextIndex.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFulLTextIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("CREATE FULLTEXT INDEX ON SearchTargets(Texts) KEY INDEX PK_SearchTargets;", suppressTransaction: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP FULLTEXT INDEX ON SearchTargets;", suppressTransaction: true);
+        }
+    }
+}
